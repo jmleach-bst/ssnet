006b79734dcd6d080cfbf5b421e3f98af346053b
Justin Leach <jleach@uab.edu>
2020-07-13 14:21:28 -0500

    added function to internally format data for using IAR priors on logit(probability of inclusion)

---
+++ R/format_iar.R
@@ -0,0 +1,34 @@
+#' Format IAR data for ssnet
+#'
+#' Given an adjacency matrix, formats data for using IAR priors to model spatial structure. Otherwise, assumes
+#' default structure where neighbors are defined as variables directly above or to the side of a specified
+#' variable location.
+#' @param x Design, or input, matrix, of dimension nobs x nvars; each row is an observation vector. It is recommended that
+#' \code{x} have user-defined column names for ease of identifying variables.
+#' @param adjmat A data.frame or matrix containing a "sparse" representation of the neighbor relationships. The first
+#' column should contain a numerical index for a given location. Each index will be repeated in this column for
+#' every neighbor it has. The indices for the location's neighbors are then specified in the second column. An optional
+#' third column specifies weights. If no third column is specified, then equal weights are assumed.
+#' @param im.res A 2-element vector where the first argument is the number of "rows" and the second argument
+#' is the number of "columns" in each subject's "image". Default is \code{NULL}.
+#'
+format_iar <- function(adjmat = NULL, im.res = NULL, x = NULL) {
+  # construct adjacency matrix
+  if (is.null(adjmat) == TRUE) {
+    if (is.null(im.res) == TRUE) {
+      stop("Require image dimensions, im.res, to create adjacency matrix.")
+    } else {
+      if (ncol(x) != prod(im.res)) {
+        stop("The product of im.res should equal the number of columns in x.")
+      }
+    }
+    adjmat <- sim2Dpredictr::proximity_builder(im.res = im.res, type = "sparse")
+  } else {
+    adjmat <- as.data.frame(adjmat)
+    colnames(adjmat)[1:2] <- c("location.index", "nb.index")
+  }
+
+  return(mungeCARdata4stan(adjmat$nb.index,
+                           table(adjmat$location.index))
+         )
+}
