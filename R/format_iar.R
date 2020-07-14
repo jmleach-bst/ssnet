a3e1d1bdeedf87cc505455d030c8ac4eb180ade5
Justin Leach <jleach@uab.edu>
2020-07-14 10:08:34 -0500

    Added ability to include manually specified tau (common precision)

--- R/format_iar.R
+++ R/format_iar.R
@@ -11,12 +11,14 @@
 #' third column specifies weights. If no third column is specified, then equal weights are assumed.
 #' @param im.res A 2-element vector where the first argument is the number of "rows" and the second argument
 #' is the number of "columns" in each subject's "image". Default is \code{NULL}.
+#' @return A list containing data formatted for using IAR priors.
 #'
-format_iar <- function(adjmat = NULL, im.res = NULL, x = NULL) {
+format_iar <- function(adjmat = NULL, im.res = NULL, x = NULL,
+                       tau.prior = "none", tau.manual = NULL) {
   # construct adjacency matrix
   if (is.null(adjmat) == TRUE) {
-    if (is.null(im.res) == TRUE) {
-      stop("Require image dimensions, im.res, to create adjacency matrix.")
+    if (is.null(im.res) == TRUE | is.null(x) == TRUE) {
+      stop("User must specify both im.res and x.")
     } else {
       if (ncol(x) != prod(im.res)) {
         stop("The product of im.res should equal the number of columns in x.")
@@ -28,7 +30,17 @@ format_iar <- function(adjmat = NULL, im.res = NULL, x = NULL) {
     colnames(adjmat)[1:2] <- c("location.index", "nb.index")
   }
 
-  return(mungeCARdata4stan(adjmat$nb.index,
-                           table(adjmat$location.index))
-         )
+  if (tau.prior == "manual") {
+    if (is.null(tau.manual) == TRUE) {
+      stop("User must specify tau.manual")
+    }
+    mcd <- mungeCARdata4stan(adjmat$nb.index,
+                             table(adjmat$location.index))
+    mcd$tau <- tau.manual
+    return(mcd)
+  } else {
+    mcd <- return(mungeCARdata4stan(adjmat$nb.index,
+                                    table(adjmat$location.index)))
+  }
+
 }
