b2d77dc1aed9c5a95659c210acbd95afe32526fa
Justin Leach <jleach@uab.edu>
2020-07-15 10:55:04 -0500

    more tests added

--- tests/testthat/test_eval_classify.R
+++ tests/testthat/test_eval_classify.R
@@ -1,3 +1,21 @@
+test_that("eval_classify throws correct errors", {
+  expect_error(eval_classify(num.class = 1),
+               "Require at least 2 classes.")
+  expect_error(eval_classify(num.class = 3, beta.hat = c("a", "b")),
+               "Parameter vector beta.hat must contain only numeric values.")
+  expect_error(eval_classify(num.class = 3, beta.hat = 1:5, y = "1"),
+               "y must contain only numeric values.")
+  expect_error(eval_classify(num.class = 2, beta.hat = 1:3,
+                             y = rnorm(10),
+                             x = data.frame(x1 = rnorm(10),
+                                            x2 = rnorm(10),
+                                            x3 = sample(x = c("a", "b"),
+                                                        size = 10,
+                                                        replace = TRUE))),
+               "x must contain only numeric values.")
+
+})
+
 test_that("vector and data frame inputs are equal", {
   expect_equal(eval_classify(beta.hat = data.frame(a = 1, b1 = 3, b2 = -3),
                              y = data.frame(s1 = 1, s2 = 0, s3 = 0, s4 = 1, s5 = 0, s6 = 1),
--- tests/testthat/test_spatial_priors.R
+++ tests/testthat/test_spatial_priors.R
@@ -1,17 +1,36 @@
 devtools::load_all()
 
-test_that("Spatial priors are correctly applied", {
+test_that("Check formating for IAR priors", {
   expect_error(ssnet(x = matrix(rnorm(100), nrow = 10, ncol = 10),
                      y = rnorm(10), family = "gaussian",
                      iar.prior = TRUE),
-               "Require image dimensions, im.res, to create adjacency matrix.")
+               "User must specify both im.res and x.")
   expect_error(ssnet(x = matrix(rnorm(10 * 16), nrow = 10, ncol = 16),
                      y = rnorm(10), family = "gaussian",
                      iar.prior = TRUE, im.res = c(5, 5)),
                "The product of im.res should equal the number of columns in x.")
   expect_error(format_iar(x = matrix(rnorm(100), nrow = 10, ncol = 10)),
-               "Require image dimensions, im.res, to create adjacency matrix.")
+               "User must specify both im.res and x.")
   expect_error(format_iar(x = matrix(rnorm(10 * 16), nrow = 10, ncol = 16),
                           im.res = c(5, 5)),
                "The product of im.res should equal the number of columns in x.")
+  expect_error(format_iar(x = matrix(rnorm(10 * 16), nrow = 10, ncol = 16),
+                          im.res = c(4, 4), tau.prior = "manual"),
+               "User must specify tau.manual")
+  expect_type(format_iar(x = matrix(rnorm(10 * 16), nrow = 10, ncol = 16),
+                         im.res = c(4, 4)),
+              "list")
+
+})
+
+test_that("Check Q2 maximization output", {
+  expect_type(max_q2_iar(iar.data = format_iar(x = matrix(rnorm(10 * 16),
+                                                          nrow = 10, ncol = 16),
+                                               im.res = c(4, 4)),
+                         p = rnorm(16)), "double")
+  expect_equal(length(max_q2_iar(iar.data = format_iar(x = matrix(rnorm(10 * 16),
+                                                                  nrow = 10, ncol = 16),
+                                                       im.res = c(4, 4)),
+                                 p = rnorm(16))),
+               length(rnorm(16)))
 })
