ebcd67f2d4fe5d5709a3733fa4e1fa7f18b7439a
Justin M Leach <jleach@uab.edu>
2023-05-12 15:20:48 -0500

    Update stan documentation

--- R/ssnet-package.R
+++ R/ssnet-package.R
@@ -11,6 +11,7 @@
 #' @importFrom rstan sampling
 #'
 #' @references
-#' Stan Development Team (2020). RStan: the R interface to Stan. R package version 2.19.3. https://mc-stan.org
+#' Stan Development Team (2020). RStan: the R interface to Stan. R package
+#' version 2.19.3. https://mc-stan.org
 #'
 NULL
